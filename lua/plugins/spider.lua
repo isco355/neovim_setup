b122dcc91ae6269dbf1f0826178df0c2071cbe3a
Francisco Ortega <franciscoortega@Francisco--MBP.attlocal.net>
2024-05-28 22:59:59 -0700

    basic image preview

---
+++ lua/plugins/spider.lua
@@ -0,0 +1,9 @@
+-- lazy.nvim
+return {
+{ "chrisgrieser/nvim-spider", lazy = true ,opts={
+    skipInsignificantPunctuation = true,
+	subwordMovement = true,
+	customPatterns = {}, -- check "Custom Movement Patterns" in the README for details
+}
+
+  },}
--- lua/polish.lua
+++ lua/polish.lua
@@ -28,3 +28,49 @@ require("neo-tree").setup({
    window = {
         position = "right",} 
 })
+
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"w",
+	"<cmd>lua require('spider').motion('w')<CR>",
+	{ desc = "Spider-w" }
+)
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"e",
+	"<cmd>lua require('spider').motion('e')<CR>",
+	{ desc = "Spider-e" }
+)
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"b",
+	"<cmd>lua require('spider').motion('b')<CR>",
+	{ desc = "Spider-b" }
+)
+
+require("neo-tree").setup({
+  filesystem = {
+    window = {
+      mappings = {
+        ["o"] = "system_open",
+      },
+    },
+  },
+  commands = {
+    system_open = function(state)
+      local node = state.tree:get_node()
+      local path = node:get_id()
+      -- macOs: open file in default application in the background.
+      vim.fn.jobstart({ "open", "-g", path }, { detach = true })
+  local p
+  local lastSlashIndex = path:match("^.+()\\[^\\]*$") -- Match the last slash and everything before it
+  if lastSlashIndex then
+  	p = path:sub(1, lastSlashIndex - 1) -- Extract substring before the last slash
+  else
+  	p = path -- If no slash found, return original path
+      end
+  vim.cmd("silent !start explorer " .. p)
+    end,
+  },
+})
+ 
