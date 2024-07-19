285ae8e66f662a31df510f362896326f9d0a744c
Francisco Ortega <franciscoortega@Francisco--MacBook-Pro.local>
2024-07-19 16:12:19 -0700

    visual surround and terminal

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -3,13 +3,15 @@ return {
     "AstroNvim/astrocore",
     ---@type AstroCoreOpts
     opts = {
-       mappings = {
+      mappings = {
         -- first key is the mode
         n = {
           -- second key is the lefthand side of the map
           -- mappings seen under group name "Buffer"
-          ["t"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "quick terminal" },
-          ["<Leader>rt"] = { "<cmd>2TermExec cmd='r' size=80 direction=vertical<cr>", desc = "quick run(terminal)" },
+          --
+          ["<C-w>"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "quick terminal" },
+          ["<Leader>rt"] = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "quick terminal" },
+          ["t"] = { "<cmd>wa | 2TermExec cmd='r' size=80 direction=vertical<cr>", desc = "quick run(terminal)" },
           ["<Leader>w"] = { "<cmd>wa<cr>", desc = "save all files" },
           ["<Leader>ya"] = { "<cmd>%y+<cr>", desc = "yank all whole file" },
         },
--- lua/polish.lua
+++ lua/polish.lua
@@ -17,36 +17,19 @@ vim.filetype.add {
   },
 }
 
-  vim.api.nvim_create_autocmd("VimEnter", {
-    command = "Neotree toggle",
-  })
-
- 
+vim.opt.relativenumber = false
+vim.api.nvim_create_autocmd("VimEnter", {
+  command = "Neotree toggle",
+})
 
 vim.o.timeoutlen = 10
 
-require("neo-tree").setup({
-   window = {
-        position = "right",
-  }
-})
+require("neo-tree").setup {
+  window = {
+    position = "right",
+  },
+}
 
-vim.keymap.set(
-	{ "n", "o", "x" },
-	"w",
-	"<cmd>lua require('spider').motion('w')<CR>",
-	{ desc = "Spider-w" }
-)
-vim.keymap.set(
-	{ "n", "o", "x" },
-	"e",
-	"<cmd>lua require('spider').motion('e')<CR>",
-	{ desc = "Spider-e" }
-)
-vim.keymap.set(
-	{ "n", "o", "x" },
-	"b",
-	"<cmd>lua require('spider').motion('b')<CR>",
-	{ desc = "Spider-b" }
-)
-
+vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
